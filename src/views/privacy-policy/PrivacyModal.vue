6a197219b2d3ca3cab3973f8daa82fa97f249fdc
rawscripter <rawscripterx@gmail.com>
2022-05-13 16:07:58 +0600

    Alt tags updated

--- src/views/privacy-policy/PrivacyModal.vue
+++ src/views/privacy-policy/PrivacyModal.vue
@@ -14,7 +14,12 @@
           </div>
           <div class="modal-body">
             <div class="logo text-center">
-              <img class="logo" width="250" src="/assets/kappi.png" alt="" />
+              <img
+                class="logo"
+                width="250"
+                src="/assets/kappi.png"
+                alt="KAPPI"
+              />
             </div>
 
             <div class="terms-contents mt-5">
