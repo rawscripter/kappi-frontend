3d6d6081555d477292065fc6a98ae2ca10120c07
rawscripter <rawscripterx@gmail.com>
2022-05-20 17:00:47 +0600

    information page updated

--- src/views/information/Information.vue
+++ src/views/information/Information.vue
@@ -2,64 +2,353 @@
   <div class="privacy-policy-page">
     <div class="page-contents text-left">
       <h5 class="mb-2">
-        <strong>Informacje</strong>
+        <strong>INFORMACJE</strong>
       </h5>
 
       <p class="list-content mb-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-        aliquid odit vero soluta architecto perferendis at natus quisquam eaque
-        neque dolorem adipisci dolor dicta, quis reprehenderit minus, eligendi
-        officia exercitationem.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit.
-        <span class="text-blue"><u>Molestias aliquid odit vero soluta</u></span>
-        architecto perferendis at natus quisquam eaque neque dolorem adipisci
-        dolor dicta, quis reprehenderit minus, eligendi officia
-        exercitationem.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Molestias aliquid odit vero soluta architecto perferendis at natus
-        quisquam eaque neque dolorem adipisci dolor dicta, quis reprehenderit
-        minus, eligendi officia exercitationem.
+        Portal <span class="brand-name">KAPPI</span> to miejsce spotkania
+        sprzedającego z interesantem na rynku nieruchomości (zarówno dla
+        transakcji sprzedaży jak i najmu). Jest to narzędzie pomagające osiągnąć
+        równowagę rynkową- biorący udział w aukcjach mogą być pewni, że zdobędą
+        swoją wymarzoną nieruchomość za najniższą możliwą w danym momencie
+        kwotę. Dzięki porównaniu ofert w czasie rzeczywistym KAPPI oferuje
+        wolnorynkową wycenę konkretnego domu czy mieszkania. KAPPI to połączenie
+        tradycyjnego modelu sprzedaży z możliwością negocjowania cen, które nie
+        są wynikiem szacunków sprzedającego, a realnego popytu na tą jedną,
+        konkretną nieruchomość. To właśnie dzięki zastosowaniu modelu aukcyjnego
+        masz pewność, że cena końcowa będzie adekwatna do wartości przedmiotu
+        licytacji.
       </p>
 
-      <div class="mb-5">
+      <!-- <div class="mb-5">
         <img class="max-100" src="/assets/img/information.jpg" alt="KAPPI" />
+      </div> -->
+
+      <div class="col-12">
+        <h3 class="mb-5 text-center">
+          <strong>Powodzenia! </strong>
+        </h3>
       </div>
 
-      <div class="mb-5">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="text-center">
+            <img
+              class="shadow p-1 info-image"
+              src="/assets/img/1.png"
+              alt="KAPPI"
+            />
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <h5 class="mb-2">
+            <strong>Jak licytować? </strong>
+          </h5>
+          <p>Od czego zacząć? Jak uzyskać dostęp do aukcji?</p>
+          <p class="pl-4 justify-content">
+            Zacznijmy od określenia, jaki typ transakcji na naszym portalu Cię
+            interesuje. Wynajem? Sprzedaż? W lewym górnym rogu znajduje
+            się zakładka o nazwie Kategorie, w której- po kliknięciu - można
+            wybrać odpowiednią opcję. Jeśli jesteś tu ze względu na konkretną
+            nieruchomość, zostałeś już dodany przez swojego Agenta (jeszcze
+            się to nie stało? - powiadom go, że założyłeś już konto!). Jeżeli
+            znalazłeś się tu i jeszcze nie jesteś pod opieką Agenta, zapraszamy
+            do kontaktu
+            <a class="mail" href="mailto:info@kappi.pl">info@kappi.pl</a>.
+          </p>
+        </div>
+      </div>
+
+      <div class="col-12 mt-5">
         <div class="row">
-          <div class="col-md-5">
+          <div class="col-md-6">
+            <h5 class="mb-2">
+              <strong>W jaki sposób złożyć ofertę? </strong>
+            </h5>
+            <p>
+              Jeśli wybrałeś już interesującą Cię aukcję z listy, możesz zacząć
+              licytować!
+            </p>
+            <p class="pl-4 justify-content">
+              W polu <strong>Twoja oferta</strong> należy wpisać deklarowaną
+              kwotę. Warto pamiętać, że minimalna wartość oferty
+              jest powiększona o ticket (kwotę postąpienia) ustalany przez
+              Agenta, stąd najniższa wartość jaka zostanie zaakceptowana przez
+              system jest sugerowana nad polem
+              <strong>Twoja oferta.</strong>
+            </p>
+          </div>
+          <div class="col-md-6">
+            <div class="text-center">
+              <img
+                class="shadow p-1 info-image"
+                src="/assets/img/2.png"
+                alt="KAPPI"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 mt-5">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="text-center">
+              <img
+                class="shadow p-1 info-image"
+                src="/assets/img/3.png"
+                alt="KAPPI"
+              />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <p class="pl-4 justify-content">
+              Po wpisaniu kwoty należy zatwierdzić swoją deklarację klikając
+              licytuj, a następnie ponownie potwierdzić złożoną ofertę. To
+              ważne, bo raz złożonej propozycji nie da się już wycofać.
+            </p>
+            <p>
+              Każdorazowo otrzymasz również wiadomość e-mail z potwierdzeniem
+              przyjęcia oferty.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 mt-5">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="pl-4 justify-content">
+              W polu <strong>Zaproponowana przez</strong> wyświetla się
+              informacja o tym, czyja oferta aktualnie wygrywa. Może to być
+              zanonimizowany e-mail lub komunikat, że ktoś inny wygrywa tę
+              aukcję. Oczywiście może pojawić się również informacja, że
+              najwyższa oferta została złożona przez Ciebie!
+            </p>
+          </div>
+          <div class="col-md-6">
+            <div class="text-center">
+              <img
+                class="shadow p-1 info-image"
+                src="/assets/img/4.png"
+                alt="KAPPI"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="row custom-mt-5">
+        <div class="col-md-12 text-center">
+          <h3 class="mb-2">
+            <strong>Uwaga! </strong>
+          </h3>
+          <div>
+            Upewnij się, że zostałeś przydzielony do prawidłowej aukcji.
+            <br />
+            Wszelkie deklaracje cenowe są
+            <u><strong>wiążące prawnie i finansowo.</strong></u>
+          </div>
+        </div>
+      </div>
+      <div class="row custom-mt-5">
+        <div class="col-md-6">
+          <div class="text-center">
+            <img
+              class="shadow p-1 info-image"
+              src="/assets/img/5.png"
+              alt="KAPPI"
+            />
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h3 class="mb-2"><strong>Gratulacje!</strong></h3>
+          <p>
+            Jeśli zobaczysz ten ekran, udało się! Jesteś zwycięzcą aukcji,
+            w której brałeś udział. Pora na kontakt z Agentem i ustalenie, jak
+            przekuć wygraną w nowy Dom!
+          </p>
+        </div>
+      </div>
+
+      <div class="row custom-mt-5">
+        <div class="col-md-6">
+          <div>
+            W przypadku gdy po zakończeniu aukcji zobaczysz taki ekran, oznacza
+            to, że złożyłeś najwyższą ofertę, ale cena minimalna ustalana przez
+            sprzedającego nie została osiągnięta. W takim wypadku skontaktuj się
+            z Agentem i zapytaj o dalsze możliwości.
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="text-center">
+            <img
+              class="shadow p-1 info-image"
+              src="/assets/img/6.png"
+              alt="KAPPI"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row custom-mt-5">
+        <div class="col-md-6">
+          <div class="text-center">
+            <img
+              class="shadow p-1 info-image"
+              src="/assets/img/7.png"
+              alt="KAPPI"
+            />
+          </div>
+        </div>
+        <div class="col-md-6">
+          <p>
+            Jeżeli Twoja oferta nie była najwyższa, na ekranie wyświetli się
+            następujący komunikat. Niestety, tym razem nie udało się wygrać, ale
+            nic straconego! Możliwe, że Twój Agent przygotował dla Ciebie inne
+            propozycje. Aby się z nimi zapoznać, sprawdź maila otrzymanego w
+            momencie zakończenia aukcji. Możesz też kliknąć w
+            <strong>Inne oferty, które mogą Cię zainteresować.</strong>
+          </p>
+        </div>
+      </div>
+
+      <div class="row custom-mt-5">
+        <div class="col-md-6">
+          <p>
+            Zobaczysz wtedy listę innych aukcji Twojego Agenta, a jeśli któraś z
+            nich wyda Ci się interesująca, <strong>poproś o dostęp.</strong>
+          </p>
+        </div>
+        <div class="col-md-6">
+          <div class="text-center">
+            <img
+              class="shadow p-1 info-image"
+              src="/assets/img/8.png"
+              alt="KAPPI"
+            />
+          </div>
+        </div>
+      </div>
+      <br /><br />
+      <div class="row custom-mt-5">
+        <div class="col-12 text-center">
+          <h4>Częste pytania:</h4>
+        </div>
+
+        <div class="mt-4">
+          <p class="m-0">
+            <strong>
+              1. Czemu czas do końca aukcji zwiększył się w ostatnich minutach
+              aukcji?
+            </strong>
+          </p>
+          <p class="m-0">
+            Jest to spowodowane parametrami ustalonymi przez Agenta związanymi z
+            aukcją kroczącą. W zależności od tego, czy w ostatnich minutach
+            trwającej aukcji któryś z użytkowników zalicytował, czas jej trwania
+            odpowiednio przedłuża się, w zależności od zmiennych ustalonych
+            przez Agenta.
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <p class="m-0">
+            <strong>
+              2. Czemu moja oferta została odrzucona, mimo iż podałem wyższą
+              kwotę niż aktualna cena?
+            </strong>
+          </p>
+          <p class="m-0">
+            Jest to spowodowane zbyt niską ofertą; w zależności od parametrów
+            ustawionych przez Agenta, aktualna cena musi być powiększona o
+            minimalny ticket (kwotę postąpienia). W przeciwnym wypadku system
+            odrzuci Twoją ofertę – ten mechanizm ma na celu urealnienie
+            licytacji i brak konieczności ciągłego przebijania ceny Twojego
+            wymarzonego Domu o 1 zł :)
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <p class="m-0">
+            <strong> 3. Wygrałem aukcję. Co dalej? </strong>
+          </p>
+          <p class="m-0">
+            Po wygranej aukcji skontaktuj się z Twoim Agentem. Najważniejsza
+            część transakcji już za Tobą – ale wciąż do wykonania zostało nieco
+            papierkowej roboty!
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <p class="m-0">
+            <strong>
+              4. Zaoferowałem najwyższą cenę za nieruchomość, ale cena minimalna
+              nie została osiągnięta. O co chodzi?</strong
+            >
+          </p>
+          <p class="m-0">
+            Może zdarzyć się tak, że cena będąca wypadkową ofert licytujących
+            nie osiągnęła minimalnego pułapu założonego przez Właściciela
+            nieruchomości. Skontaktuj się ze swoim Agentem – może wspólnie uda
+            się Wam przekonać drugą stronę do zamknięcia transakcji po
+            oferowanej przez Ciebie cenie.
+          </p>
+        </div>
+
+        <div class="mt-4">
+          <p class="m-0">
+            <strong>
+              5. Moja oferta nie była najwyższa – co mogę zrobić dalej?</strong
+            >
+          </p>
+          <p class="m-0">
+            Niestety, zwycięzca aukcji może być tylko jeden. KAPPI wraz z Twoim
+            Agentem nie zostawiają Cię jednak samego w dalszych poszukiwaniach
+            wymarzonego Domu! Sprawdź maila, którego otrzymałeś od nas po
+            zakończeniu aukcji – znajdują się tam inne perełki wskazane przez
+            Agenta, które mogą Cię zainteresować. Możliwe, że w najbliższym
+            czasie zaplanowana jest inna aukcja Twojego Agenta. Kto wie, może
+            któraś z nich okaże się dla Ciebie ciekawa? W takim wypadku kliknij
+            na <strong>Inne oferty, które mogą Cię zainteresować</strong> w
+            oknie zakończonej aukcji i zobacz jakie nieruchomości będą
+            licytowane w najbliższym czasie – a jeśli coś przykuje Twoją uwagę,
+            kliknij <strong>Poproś o dostęp</strong>, lub po prostu skontaktuj
+            się ze swoim Agentem :)
+          </p>
+        </div>
+      </div>
+
+      <div class="col-12 mt-5 text-center">
+        w przypadku jakichkolwiek innych pytań – jesteśmy do Twojej dyspozycji
+        pod adresem<br />
+        <a class="mail" href="mailto:info@kappi.pl">info@kappi.pl</a>.
+      </div>
+      <!-- <div class="mb-5">
+        <div class="row">
+          <div class="col-md-6">
             <img
               class="max-400 shadow p-1"
               src="/assets/img/auctions.png"
               alt="KAPPI"
             />
           </div>
-          <div class="col-md-7">
+          <div class="col-md-6">
+            <h5 class="mb-2">
+              <strong>Jak licytować? </strong>
+            </h5>
+            <p>Od czego zacząć? Jak uzyskać dostęp do aukcji?</p>
             <p class="pl-4 justify-content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              aliquid odit vero soluta architecto perferendis at natus quisquam
-              eaque neque dolorem adipisci dolor dicta, quis reprehenderit
-              minus, eligendi officia exercitationem.Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. architecto perferendis at natus
-              quisquam eaque neque dolorem adipisci dolor dicta, quis
-              reprehenderit minus, eligendi officia exercitationem.
-
-              <br />
-              <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              aliquid odit vero soluta architecto perferendis at natus quisquam
-              eaque neque dolorem adipisci dolor dicta, quis reprehenderit
-              minus, eligendi officia exercitationem.
-              <br />
-              <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              aliquid odit vero soluta architecto perferendis at natus quisquam
-              eaque neque dolorem adipisci dolor dicta, quis reprehenderit
-              minus, eligendi officia exercitationem. minus, eligendi officia
-              exercitationem. eaque neque dolorem adipisci dolor dicta, quis
-              reprehenderit minus, eligendi officia exercitationem. minus,
-              eligendi officia exercitationem. exercitationem. eaque neque
-              dolorem adipisci dolor dicta, quis reprehenderit minus, eligendi
-              officia exercitationem. minus, eligendi officia exercitationem.
+              Zacznijmy od określenia, jaki typ transakcji na naszym portalu Cię
+              interesuje. Wynajem? Sprzedaż? W lewym górnym rogu znajduje
+              się zakładka o nazwie Kategorie, w której- po kliknięciu - można
+              wybrać odpowiednią opcję. Jeśli jesteś tu ze względu na konkretną
+              nieruchomość, zostałeś już dodany przez swojego Agenta (jeszcze
+              się to nie stało? - powiadom go, że założyłeś już konto!). Jeżeli
+              znalazłeś się tu i jeszcze nie jesteś pod opieką Agenta,
+              zapraszamy do kontaktu info@kappi.com.
             </p>
           </div>
         </div>
@@ -67,7 +356,7 @@
 
       <div class="mb-5">
         <div class="row">
-          <div class="col-md-7">
+          <div class="col-md-6">
             <p class="pl-4 justify-content">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
               aliquid odit vero soluta architecto perferendis at natus quisquam
@@ -96,7 +385,7 @@
               officia exercitationem. minus, eligendi officia exercitationem.
             </p>
           </div>
-          <div class="col-md-5">
+          <div class="col-md-6">
             <img
               class="max-400 shadow p-3"
               src="/assets/img/auction-details.png"
@@ -120,12 +409,12 @@
             >www.yarmobile.com</a
           >
         </p>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
  <script scop>
-</script> 
+</script>
 
 <style>
 img {
@@ -157,5 +446,22 @@ img {
   text-decoration: none;
   color: rgb(96, 96, 96);
   font-weight: 600;
+}
+.info-image {
+  width: 100%;
+}
+
+p,
+div {
+  font-size: 17px;
+  line-height: 30px;
+}
+.custom-mt-5 {
+  margin-top: 45px !important;
+}
+
+a.mail {
+  color: #3f7ff0 !important;
+  text-decoration: underline !important;
 }
 </style>
